7fec9973a6cb914cce8d5ef07bad1c9c2c2eed1d
cs-lucas-nascimento <l.b.do.nascimento@accenture.com>
2020-01-31 19:25:16 -0300

    Add module List

---
+++ GenerambaExample/Classes/Modules/List/ViewController/CBListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ListCBListViewController.swift
+//  GenerambaExample
+//
+//  Created by cs-lucas-nascimento on 31/01/2020.
+//  Copyright © 2020 Concrete. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+
+    // MARK: - Life cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ GenerambaExampleTests/Classes/Modules/List/ViewController/CBListViewControllerTests.swift
@@ -0,0 +1,23 @@
+//
+//  CBListCBListViewControllerTests.swift
+//  GenerambaExample
+//
+//  Created by cs-lucas-nascimento on 31/01/2020.
+//  Copyright © 2020 Concrete. All rights reserved.
+//
+
+import XCTest
+
+class ListViewControllerTest: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+}
